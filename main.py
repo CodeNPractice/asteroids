69fee5eff8074abf35e458361342d4ecb923e4e7
CodeNPractice <abduraoufaryan@gmail.com>
2026-07-11 12:20:48 -0700

    add rotation

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
    pygame.init()
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
+   clock = pygame.time.Clock()
+   dt = 0.0
+   player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
    while(True): 
        log_state()
        for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
+
+       player.update(dt)   
+
        screen.fill("black")
+       player.draw(screen)
        pygame.display.flip()
+
+
+       #limit the framerate to 60 fps
+       dt = clock.tick(60) / 1000
       
 if __name__ == "__main__":
     main()
